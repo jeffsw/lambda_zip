0cf224617513f06dde6687f848237e637ff60d7a
Jeff Wheeler <jeffsw6@gmail.com>
2023-01-11 07:50:02 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='1.23011101',
+    version='1.23011102',
     name = 'lambda_zip',
     author = 'Jeff Wheeler',
     author_email = 'jeffsw6@gmail.com',
